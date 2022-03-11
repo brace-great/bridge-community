6842c64b8ea625073265c26434e13451dbd0bc5d
brace-great <2328587199@qq.com>
2022-03-11 22:54:50 +0800

    0.0.0

--- ui/src/pages/Auth.vue
+++ ui/src/pages/Auth.vue
@@ -20,19 +20,20 @@
 
         <q-tab-panels v-model="tab" animated>
           <q-tab-panel name="mails">
-            <div class="text-h6">Mails</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <q-input v-model="regForm.account" label="account" />
+            <q-input v-model="regForm.passwordText" label="password" />
+            <q-btn
+              color="white"
+              text-color="black"
+              label="Standard"
+              @click="register"
+            />
+            <q-btn color="white" text-color="black" label="Standard" />
           </q-tab-panel>
 
-          <q-tab-panel name="alarms">
-            <div class="text-h6">Alarms</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </q-tab-panel>
+          <q-tab-panel name="alarms"> </q-tab-panel>
 
-          <q-tab-panel name="movies">
-            <div class="text-h6">Movies</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </q-tab-panel>
+          <q-tab-panel name="movies"> </q-tab-panel>
         </q-tab-panels>
       </q-card>
 
@@ -73,12 +74,30 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref, reactive } from "vue";
+import axios from "axios";
 
 export default defineComponent({
   name: "Auth",
   setup() {
+    const regForm = reactive({
+      accountText: "",
+      passwordText: "",
+    });
+
+    const register = async () => {
+      const register2 = (account, password) => {
+        axios.post("http://127.0.0.1:3333/api/auth/register", {
+          account,
+          password,
+        });
+      };
+      const res = await register2(regForm.account, regForm.password);
+      console.log(res);
+    };
     return {
+      regForm,
+      register,
       tab: ref("mails"),
     };
   },
